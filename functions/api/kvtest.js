98b0537f6f0ccc25ee4f5df4d144f4c24e1eb2b7
Takumi Abe <start.7185.herausforderung@gmail.com>
2023-07-21 12:56:30 +0900

    wip

--- functions/api/kvtest.js
+++ functions/api/kvtest.js
@@ -1,4 +1,9 @@
 export async function onRequest(context) {
+    console.log("###1 is", context);
+    console.log("###2 is", context.env);
+    console.log("###3 is", env);
+    console.log("###4 is", env.TODO_LIST);
+    console.log("###5 is", TODO_LIST);
     const task = await context.env.TODO_LIST.get("test");
     return new Response(task);
 }
